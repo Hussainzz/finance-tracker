46aabcf193db82df6d6618494327afc6ca1f80a4
Rails udemy <rails@gmail.com>
2018-05-22 03:48:02 -0400

    Add user and stock association

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search" 
+  resources :user_stocks, only: [:create, :destroy]
   
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
